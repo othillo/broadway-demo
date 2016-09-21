bcd3602f0cb0c9d15690e9f9fe944ce2700d7878
Gaultier Boniface <gboniface@genedys.com>
2016-09-21 14:52:10 +0200

    Optimize the whole setup to work correctly in docker.

--- run.sh
+++ run.sh
@@ -3,8 +3,13 @@
 # install dependencies
 composer install --ignore-platform-reqs # ignore required mongo PHP extension as it is not used in this application
 
+# update app cache
+app/console ca:cl
+app/console ca:wa
+
 # initialize the database
 app/console broadway:event-store:schema:init
 
 # run the app
-app/console server:run -vvv --env=prod 0.0.0.0:8000
+#app/console server:run -vvv 0.0.0.0:8000
+php -S 0.0.0.0:8000 web/app.php
